aeab7d8b196205335e12a8fd474e52522b7e85b1
Vania Kurniawati <koala@MacBook-Air.local>
2015-01-05 13:02:05 -0800

    Initial Commit

---
+++ TweetFellow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetFellow
+//
+//  Created by Vania Kurniawati on 1/5/15.
+//  Copyright (c) 2015 Vania Kurniawati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
